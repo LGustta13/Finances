03e1b4d7632f97527ea6ce63afd976d6b980d33b
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-03 20:36:01 -0300

    Added styled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export const Dashboard = () => {
+    return (
+        <Container>
+            <Title>
+                Dashboard
+            </Title>
+        </Container>
+    )
+};

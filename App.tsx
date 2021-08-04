bc208fdc618f016625b1a9678040604f28b5745c
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-04 19:41:49 -0300

    Added ThemeProvider and its context

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
 
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar
         hidden={true}
       />
 
       <Dashboard />
-    </>
+    </ThemeProvider>
 
   )
 }

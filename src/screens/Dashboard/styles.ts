a699f2f4f8cca0127250d79052208528027afce7
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-04 19:43:09 -0300

    Created a styled-component with colors related to provided global theme

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
+
+    background-color: ${(provider) => provider.theme.colors.secondary};
 `;
 
 export const Title = styled.Text`
     font-size: 30px;
     font-weight: bold;
-    color: darkblue;
+    color: ${(provider) => provider.theme.colors.title};
 `;

cb974f2949738bd4081b5c02d81e0ff22ccdb3f7
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-04 19:34:29 -0300

    Properties for color themes configurated, used as global

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+    colors: {
+        primary: '#5636d3',
+
+        secondary: '#FF872C',
+        secondary_light: 'rgba(255, 135, 44, 0.3)', // o a é opacidade
+    
+        success: '#12a454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#e83f5b',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#ffffff',
+        title: '#363f5f',
+        text: '#969cb2',
+        background: '#f0f2f5'
+    }
+}
+
+export default theme;

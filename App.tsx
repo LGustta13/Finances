c2d292055f9694c1bca5aa33378d6a0ca41b6fe9
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-05 18:33:37 -0300

    Status bar component removed

--- App.tsx
+++ App.tsx
@@ -27,9 +27,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        hidden={true}
-      />
+     
 
       <Dashboard />
     </ThemeProvider>

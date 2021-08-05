403dc49cd21d6752bd81677a0cbb1432e279794e
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-05 18:32:52 -0300

    Added new styled components (Container to UserName) and responsive fonts and sizes for them

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,14 +1,36 @@
+// CORE
 import React from 'react';
 import { } from 'react-native';
 
-import { Container, Title } from './styles';
+// COMMUNITY
+
+
+// MINE
+import { 
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreetings,
+    UserName
+} from './styles';
+
 
 export const Dashboard = () => {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserInfo>
+                    <Photo source={{uri: 'https://avatars.githubusercontent.com/u/67337459?v=4'}}/>
+                    <User>
+                        <UserGreetings>Olá, </UserGreetings>
+                        <UserName>Luis Gustavo</UserName>
+                    </User>
+                </UserInfo>
+
+            </Header>
+            
         </Container>
     )
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,16 +1,50 @@
+// CORE
+
+// COMMUNITY
 import styled from 'styled-components/native';
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-
-    background-color: ${(provider) => provider.theme.colors.secondary};
-`;
+// MINE
 
 // Para acessar deve executar uma array function
-export const Title = styled.Text`
-    font-family: ${(object) => object.theme.fonts.bold}; 
-    font-size: 30px;
-    color: ${(provider) => provider.theme.colors.title};
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${(provider) => provider.theme.colors.background};
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({theme}) => theme.colors.primary};
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 0 24px;
+    width: 100%
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 18px;
+`;
+
+export const UserGreetings = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.bold};
 `;
